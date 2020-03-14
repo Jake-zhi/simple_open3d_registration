71f9af617982acc7dfcab389f4a10ff91e42ec6e
1080_pc <hoge@hoge.com>
2020-03-14 15:04:34 +0900

    update 0.9.0.0

--- simple_registration.py
+++ simple_registration.py
@@ -15,6 +15,7 @@ import sys
 
 import numpy as np
 import open3d as o3d
+print(o3d.__version__)
 
 
 def get_args(arg_num):
@@ -32,8 +33,8 @@ class SimpleRegistration():
     
     def read_point_clouds(self, SOURCE_PATH, TARGET_PATH):
         "read point clouds"
-        source = o3d.read_point_cloud(SOURCE_PATH)
-        target = o3d.read_point_cloud(TARGET_PATH)
+        source = o3d.io.read_point_cloud(SOURCE_PATH)
+        target = o3d.io.read_point_cloud(TARGET_PATH)
         transform_init = np.asarray([[0.0, 0.0, 1.0, 0.0], [1.0, 0.0, 0.0, 0.0],
                                     [0.0, 1.0, 0.0, 0.0], [0.0, 0.0, 0.0, 1.0]])
         source.transform(transform_init)
@@ -42,17 +43,16 @@ class SimpleRegistration():
     def preprocess_point_cloud(self, pcd, voxel_size=0.01):
         "preprocess point cloud"
         print(":: Downsample with a voxel size %.3f." % voxel_size)
-        pcd_down = o3d.voxel_down_sample(pcd, voxel_size)
+        pcd_down = pcd.voxel_down_sample(voxel_size)
 
         radius_normal = voxel_size * 2
         print(":: Estimate normal with search radius %.3f." % radius_normal)
-        o3d.estimate_normals(pcd_down,
-                            o3d.KDTreeSearchParamHybrid(radius=radius_normal, max_nn=30))
+        pcd_down.estimate_normals(o3d.geometry.KDTreeSearchParamHybrid(radius=radius_normal, max_nn=30))
 
         radius_feature = voxel_size * 5
         print(":: Compute FPFH feature with search radius %.3f." % radius_feature)
-        pcd_fpfh = o3d.compute_fpfh_feature(pcd_down,
-                                            o3d.KDTreeSearchParamHybrid(radius=radius_feature, max_nn=100))
+        pcd_fpfh = o3d.registration.compute_fpfh_feature(pcd_down,
+                                            o3d.geometry.KDTreeSearchParamHybrid(radius=radius_feature, max_nn=100))
         return pcd_down, pcd_fpfh
 
     def execute_global_registration(self, source, target, voxel_size=0.01):
@@ -64,12 +64,12 @@ class SimpleRegistration():
         print(":: RANSAC registration on downsampled point clouds.")
         print("   Since the downsampling voxel size is %.3f," % voxel_size)
         print("   we use a liberal distance threshold %.3f." % DISTANCE_THRES)
-        self.ransac_result = o3d.registration_ransac_based_on_feature_matching(
+        self.ransac_result = o3d.registration.registration_ransac_based_on_feature_matching(
                             source_down, target_down, source_fpfh, target_fpfh, DISTANCE_THRES,
-                            o3d.TransformationEstimationPointToPoint(False), 4, 
-                            [o3d.CorrespondenceCheckerBasedOnEdgeLength(0.9),
-                            o3d.CorrespondenceCheckerBasedOnDistance(DISTANCE_THRES)],
-                            o3d.RANSACConvergenceCriteria(4000000, 500))
+                            o3d.registration.TransformationEstimationPointToPoint(False), 4, 
+                            [o3d.registration.CorrespondenceCheckerBasedOnEdgeLength(0.9),
+                            o3d.registration.CorrespondenceCheckerBasedOnDistance(DISTANCE_THRES)],
+                            o3d.registration.RANSACConvergenceCriteria(4000000, 500))
         dst_source.transform(self.ransac_result.transformation)
         return dst_source
     
@@ -80,8 +80,8 @@ class SimpleRegistration():
         print(":: Point-to-plane ICP registration is applied on original point")
         print("   clouds to refine the alignment. This time we use a strict")
         print("   distance threshold %.3f." % DISTANCE_THRES)
-        self.icp_result = o3d.registration_icp(source, target, DISTANCE_THRES, self.ransac_result.transformation,
-                                        o3d.TransformationEstimationPointToPlane())
+        self.icp_result = o3d.registration.registration_icp(source, target, DISTANCE_THRES, self.ransac_result.transformation,
+                                        o3d.registration.TransformationEstimationPointToPlane())
         dst_source.transform(self.icp_result.transformation)
         return dst_source
 
@@ -93,14 +93,14 @@ class SimpleRegistration():
             source_temp.paint_uniform_color([1, 0.706, 0])
             target_temp.paint_uniform_color([0, 0.651, 0.929])
         source_temp.transform(transform)
-        o3d.draw_geometries([source_temp, target_temp])
+        o3d.visualization.draw_geometries([source_temp, target_temp])
         return
     
     def save_coalition_3d(self, source, target, OUTPUT_PATH):
         "save point cloud"
         dst = source + target
         basename_without_ext = os.path.splitext(os.path.basename(OUTPUT_PATH))[0]
-        o3d.write_point_cloud(basename_without_ext + "_dst.ply", dst)
+        o3d.io.write_point_cloud(basename_without_ext + "_dst.ply", dst)
         return
 
 class Simple3DFilter():
